67c3b989bb3bab19d6f278b820e99e1fbd97088e
Jmajane <jordan.majane@outlook.com>
2022-06-06 09:36:10 -0400

    home page refactored

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
-class Home(View):
 
-    def get(self, request):
-        return HttpResponse("Noms Home")
+class Home(TemplateView):
+    template_name = "home.html"
 
 
-class About(View):
+class About(TemplateView):
 
-    def get(self, request):
-        return HttpResponse("About Noms")
+    template_name = "about.html"
 
 
